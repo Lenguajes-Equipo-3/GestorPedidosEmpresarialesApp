c1f89d08175f5777582492138aed5d02f7e34d51
YarexUCR <yarexespinoza02@gmail.com>
2025-07-01 11:17:45 -0600

    View parramenrros funcional

---
+++ GestorPedidosEmpresarialesApp/Services/ParametroSistemaService.cs
@@ -0,0 +1,55 @@
+﻿using GestorPedidosEmpresarialesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GestorPedidosEmpresarialesApp.Services
+{
+    public class ParametroSistemaService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _baseUrl = AppConfig.GetApiBaseUrl();
+
+        public ParametroSistemaService()
+        {
+            if (_httpClient.BaseAddress == null && !string.IsNullOrEmpty(_baseUrl))
+                _httpClient.BaseAddress = new Uri(_baseUrl);
+        }
+
+        // GET: Obtener los parámetros
+        public async Task<ParametroSistema?> GetParametrosAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/parametrossistema");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ParametroSistema>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+            return null;
+        }
+
+        // POST: Crear parámetros
+        public async Task<string> AddParametrosAsync(ParametroSistema parametros)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/api/parametrossistema", parametros);
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+
+        // PUT: Actualizar parámetros
+        public async Task<string> UpdateParametrosAsync(ParametroSistema parametros)
+        {
+            var response = await _httpClient.PutAsJsonAsync("/api/parametrossistema", parametros);
+            var json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+    }
+}
--- GestorPedidosEmpresarialesApp/Utils/SesionActual.cs
+++ GestorPedidosEmpresarialesApp/Utils/SesionActual.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestorPedidosEmpresarialesApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,10 @@ namespace GestorPedidosEmpresarialesApp.Utils
     public static class SesionActual
     {
         public static Usuario UsuarioLogueado { get; set; }  // Inicializar para evitar null
+        public static ParametroSistema? ParametrosSistema { get; set; }
+
     }
+
+
+
 }
