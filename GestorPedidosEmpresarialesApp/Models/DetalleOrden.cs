4943891469051d2033a6932fb6f90e38787b2cd2
YarexUCR <yarexespinoza02@gmail.com>
2025-07-02 02:27:34 -0600

    Modelo y services

--- GestorPedidosEmpresarialesApp/Models/DetalleOrden.cs
+++ GestorPedidosEmpresarialesApp/Models/DetalleOrden.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestorPedidosEmpresarialesApp.Models
 {
-    internal class DetalleOrden
+    public class DetalleOrden : INotifyPropertyChanged
     {
+        private int idDetalleOrden;
+        public int IdDetalleOrden
+        {
+            get => idDetalleOrden;
+            set { idDetalleOrden = value; OnPropertyChanged(); }
+        }
+
+        private ProductoVariante varianteProducto;
+        public ProductoVariante VarianteProducto
+        {
+            get => varianteProducto;
+            set { varianteProducto = value; OnPropertyChanged(); }
+        }
+
+        private int cantidad;
+        public int Cantidad
+        {
+            get => cantidad;
+            set { cantidad = value; OnPropertyChanged(); }
+        }
+
+        private double precioLinea;
+        public double PrecioLinea
+        {
+            get => precioLinea;
+            set { precioLinea = value; OnPropertyChanged(); }
+        }
+
+        private bool eliminado;
+        public bool Eliminado
+        {
+            get => eliminado;
+            set { eliminado = value; OnPropertyChanged(); }
+        }
+
+        // Notificación de cambios para WPF
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
